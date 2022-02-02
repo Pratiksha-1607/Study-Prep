4b0fe9739dc26dbad236cbb47e683d8861a9af8b
Pratiksha-1607 <90341835+Pratiksha-1607@users.noreply.github.com>
2022-02-03 02:10:55 +0530

    Update conn.php

--- Focus-Time/conn.php
+++ Focus-Time/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost", "root", "", "tms");
+	$conn = new mysqli("studprep.mysql.database.azure.com", "studprep@studprep", "Pratiksha@1607", "tms");
  
 	if(!$conn){
 		die("Error: Cannot connect to the database");
